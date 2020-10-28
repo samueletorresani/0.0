fc81851376a25d5a286cd270d6241ea158223d40
Samuele Torresani <samuele.torresani01@gmail.com>
2020-10-28 14:27:16 +0100

    initial commit

---
+++ 0.8/0.8.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a,b;
+    cout<<"inserisci a ";
+    cin>> a;
+    cout<<"inserisci b ";
+    cin>> b;
+
+    if (a>=b)
+        if (a%b==0)
+            cout<<"a multiplo di b";
+        else 
+            cout<<"a NON multiplo di b";
+    else
+        cout<<"a non multiplo di b perche' minore";
+return 0; 
+}
